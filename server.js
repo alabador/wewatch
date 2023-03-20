67e6c6aa933b0b23d01954da5f00f4f1a33bbbe3
alabador <austinlabador@gmail.com>
2023-03-19 18:59:37 -0700

    Add more setup to server

--- server.js
+++ server.js
@@ -1,3 +1,20 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config({path: './config/.env'});
+
+// COnnect to DB
 
 
-require('dotenv').config({path: './config/.env'})
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+// Routes
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on Port ${process.env.PORT}.`)
+})
