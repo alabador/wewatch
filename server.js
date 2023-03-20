328941a0ae6e3bd8e4b13e9154035fd9a800f898
alabador <austinlabador@gmail.com>
2023-03-19 19:41:52 -0700

    Add controller, routes, and views folders. Setup first path to home route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const homeRoutes = require('./routes/home')
 
 require('dotenv').config({path: './config/.env'});
 
@@ -13,7 +14,7 @@ app.use(express.json());
 
 
 // Routes
-
+app.use('/', homeRoutes)
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on Port ${process.env.PORT}.`)
