f5501584415f0750c013d23f2abab1c4d1872a28
alabador <austinlabador@gmail.com>
2023-03-21 02:31:06 -0700

    Change style of card, add image and info from api

--- controllers/anime.js
+++ controllers/anime.js
@@ -6,11 +6,9 @@ module.exports = {
         res.render('animeSearch.ejs')
     },
     getAnimeSearchResults: async (req, res) => {
-        console.log('banana')
         let show = req.params.name;
         // res.send(`Anime ${show}`)
         const apiData = await animeAPI.searchByName(show)
-        console.log(apiData.data.length)
         res.render('animeSearch.ejs', {searchResults: apiData.data})
     },
 }
