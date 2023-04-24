c8a8518954c960668ed3151b45ecae0f713c41ff
Austin Labador <austinlabador@gmail.com>
2023-04-24 06:17:16 -0700

    Add config files, edit routes for login flow, and create signup/login pages

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose') //helps us use models, better interaction with db
+const passport = require('passport') //helps us with auth, has strategies for different types of auth
+const session = require('express-session') // cookies - allows us to keep users logged in
+const MongoStore = require('connect-mongo')(session) //allows us to store session in mongodb
+const flash = require('express-flash') //flashes a message when validation fails
+const logger = require('morgan') //helps log all the requests in terminal
 const connectDB = require('./config/database')
 const homeRoutes = require('./routes/home')
 const animeRoutes = require('./routes/anime')
+const mainRoutes = require('./routes/main')
 
 require('dotenv').config({path: './config/.env'});
+
+require('./config/passport')(passport)
 
 // Connect to DB
 connectDB()
@@ -13,10 +22,27 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+app.use(logger('dev'))
 
+// Sessions
+app.use(
+    session({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false,
+      store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    })
+  )
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 
 // Routes
-app.use('/', homeRoutes)
+app.use('/', mainRoutes)
+app.use('/home', homeRoutes)
 app.use('/anime', animeRoutes)
 
 app.listen(process.env.PORT, () => {
